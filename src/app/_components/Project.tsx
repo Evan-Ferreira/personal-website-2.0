c79620b00a14c1f1d93ca535c64123592a6cde76
Evan Ferreira <evanjfer@gmail.com>
2025-03-02 16:57:24 -0500

    wip: all elements and positioning for project components

--- src/app/_components/Project.tsx
+++ src/app/_components/Project.tsx
@@ -1,28 +1,74 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { Host_Grotesk } from 'next/font/google';
+
+const hostGrotesk = Host_Grotesk({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700'],
+});
 
 const Project = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <div className="">
             <Link href="https://github.com/Evan-Ferreira/Stumble_HackTheHill2024">
-                <div className="relative">
+                <div
+                    className="relative hover:scale-[105%]"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
                     <img src="/project1.png" alt="Project 1" width="100%" />
-                    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#2A2A2A]"></div>
-                    <div className="absolute bottom-0 w-full flex px-4 py-2 gap-4">
-                        <img
-                            src="/react.png"
-                            alt="React"
-                            width="25"
-                            height="25"
-                        />
-                        <img src="/git.png" alt="Git" width="25" height="25" />
-                        <img
-                            src="/firebase.png"
-                            alt="Firebase"
-                            width="25"
-                            height="25"
-                        />
-                    </div>
+                    {isHovered && (
+                        <>
+                            <div className="absolute inset-0 bg-gradient-to-b from-[#2A2A2A]/50 to-[#2A2A2A]"></div>
+                            <div className="absolute inset-0 flex flex-col h-full w-full">
+                                <p
+                                    className={`${hostGrotesk.className} font-light px-4 py-2`}
+                                >
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Quisquam, quos.
+                                </p>
+                                <div className="w-full flex justify-between px-4 py-2 mt-auto">
+                                    <div className="flex gap-4">
+                                        <img
+                                            src="/react.png"
+                                            alt="React"
+                                            width="25"
+                                            height="25"
+                                        />
+                                        <img
+                                            src="/git.png"
+                                            alt="Git"
+                                            width="25"
+                                            height="25"
+                                        />
+                                        <img
+                                            src="/firebase.png"
+                                            alt="Firebase"
+                                            width="25"
+                                            height="25"
+                                        />
+                                    </div>
+                                    <div className="flex gap-2">
+                                        <p
+                                            className={`${hostGrotesk.className} font-light`}
+                                        >
+                                            Learn More
+                                        </p>
+                                        <img
+                                            src="/right-arrow-icon.svg"
+                                            alt="Right Arrow"
+                                            width="24"
+                                            height="24"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </div>
             </Link>
         </div>
