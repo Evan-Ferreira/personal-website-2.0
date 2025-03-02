6b283d177f303ce35ef3c8d958593d309105666d
Evan Ferreira <evanjfer@gmail.com>
2025-03-02 02:52:29 -0500

    feat: clickable resume items

--- src/app/_components/Resume.tsx
+++ src/app/_components/Resume.tsx
@@ -1,49 +1,70 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import WorkExperience from './resume/WorkExperience';
 
 const Resume = () => {
+    const [activeTab, setActiveTab] = useState<number | null>(null);
+
     return (
         <div className="flex flex-col w-full gap-6">
             <WorkExperience
+                id={0}
                 title="Software Developer"
                 company="Taiv (YC W20)"
                 description="Series A ad-tech startup focused on real-time TV commercial detection."
                 startDate="2024"
                 endDate="Present"
+                setActiveTab={setActiveTab}
+                activeTab={activeTab}
             />
             <WorkExperience
+                id={1}
                 title="Software Developer"
                 company="Pillexa"
                 description="D2C healthcare platform focused on quickly scaling e-commerce health brands."
                 startDate="2024"
                 endDate="Present"
+                setActiveTab={setActiveTab}
+                activeTab={activeTab}
             />
             <WorkExperience
+                id={2}
                 title="Computer Vision Research Intern"
                 company="Queen's University"
                 description="Algorithms, architectures, and transfer learning for real-time TV commercial detection."
                 startDate="2024"
+                setActiveTab={setActiveTab}
+                activeTab={activeTab}
             />
             <WorkExperience
+                id={3}
                 title="Co-Founder"
                 company="Local Reach - acq. by Taiv (YC W20)"
                 description="Ad-tech startup leveraging overlooked restaurant TV advertising."
                 startDate="2024"
                 endDate="2025"
+                setActiveTab={setActiveTab}
+                activeTab={activeTab}
             />
             <WorkExperience
+                id={4}
                 title="Executive Director"
                 company="inQUbate"
                 description="Canada's leading undergraduate student startup accelerator."
                 startDate="2024"
                 endDate="2025"
+                setActiveTab={setActiveTab}
+                activeTab={activeTab}
             />
             <WorkExperience
+                id={5}
                 title="Athlete"
                 company="Queen's Varsity Wrestling Team"
                 description="National competitor representing Queen's University."
                 startDate="2022"
                 endDate="Present"
+                setActiveTab={setActiveTab}
+                activeTab={activeTab}
             />
         </div>
     );
--- src/app/_components/resume/WorkExperience.tsx
+++ src/app/_components/resume/WorkExperience.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 
 type WorkExperienceProps = {
+    id: number;
     title: string;
     company: string;
     description: string;
     startDate: string;
     endDate?: string;
+    setActiveTab: (tab: number | null) => void;
+    activeTab: number | null;
 };
 
 const WorkExperience = ({
+    id,
     title,
     company,
     description,
     startDate,
     endDate,
+    setActiveTab,
+    activeTab,
 }: WorkExperienceProps) => {
     return (
-        <div className="flex flex-col">
+        <div
+            className="flex flex-col hover:cursor-pointer"
+            onClick={
+                id === activeTab
+                    ? () => setActiveTab(null)
+                    : () => setActiveTab(id)
+            }
+        >
             <div className="flex gap-4 items-center">
                 <h3 className="text-lg">{title}</h3>
                 <div className="flex-grow h-[0.1px] bg-[#969696]"></div>
@@ -24,8 +37,11 @@ const WorkExperience = ({
                     {startDate} {endDate ? `- ${endDate}` : ''}
                 </h2>
             </div>
+
             <p className="text-base text-[#969696] mb-2">{company}</p>
-            <p className="text-sm text-[#969696] italic">{description}</p>
+            {activeTab === id && (
+                <p className="text-sm text-[#969696] italic">{description}</p>
+            )}
         </div>
     );
 };
