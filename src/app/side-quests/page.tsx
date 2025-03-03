d6d9cd6fc93a8dd894c4cb4fcf4aa18b49357238
Evan Ferreira <evanjfer@gmail.com>
2025-03-02 19:17:51 -0500

    feat: title and description for side-quests page

--- src/app/side-quests/page.tsx
+++ src/app/side-quests/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const page = () => {
-    return <div>page</div>;
+const SideQuests = () => {
+    return (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+            <div className="flex flex-col w-[28%] items-center">
+                <h3 className="text-[#969696] text-md">EVAN FERREIRA</h3>
+                <h1 className="text-white text-5xl font-bold  mt-2 mb-3">
+                    SIDE QUESTS
+                </h1>
+                <p className="text-[#E9E9E9] text-sm">
+                    I like to keep myself busy — maybe too busy. I have a lot of
+                    hobbies and side projects and when I'm curious about
+                    something, I dive deep into it. Here you'll find anything
+                    related to software, athletics, and anything in between.
+                </p>
+            </div>
+        </div>
+    );
 };
 
-export default page;
+export default SideQuests;
