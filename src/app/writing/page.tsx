1a5163d04e198c06ab1b9c793fb1cf78f86b1677
Evan Ferreira <evanjfer@gmail.com>
2025-03-05 22:08:28 -0800

    feat: boilerplate for writing page

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const page = () => {
-    return <div>page</div>;
+    return (
+        <div className="flex flex-col items-center min-h-screen pb-16 pt-24 px-48 gap-16 bg-[#2A2A2A]">
+            <div className="flex flex-col w-96 items-center">
+                <h3 className="text-[#969696] text-md">EVAN FERREIRA</h3>
+                <h1 className="text-white text-5xl font-bold  mt-2 mb-3">
+                    WRITING
+                </h1>
+                <p className="text-[#E9E9E9] text-sm">
+                    If I'm being fully honest, I'm not a great writer and I'm
+                    not sure I'm disciplined enough to consistently write.
+                    However, I like to think writing during my (little) free
+                    time is a good way to force myself to reflect on my thoughts
+                    and experiences.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default page;
