c2193b90a4d94c7acb7f7e8b4170f86c9cb87e3a
Evan Ferreira <evanjfer@gmail.com>
2025-03-06 00:44:36 -0800

    feat: mobile view compatible

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -3,7 +3,7 @@ import Article from '../_components/Article';
 
 const page = () => {
     return (
-        <div className="flex flex-col items-center min-h-screen pb-16 pt-32 px-48 gap-16 bg-[#2A2A2A]">
+        <div className="flex flex-col items-center min-h-screen pb-16 pt-32 px-16 lg:px-48 gap-16 bg-[#2A2A2A]">
             <div className="flex flex-col w-96 items-center">
                 <h3 className="text-[#969696] text-md">EVAN FERREIRA</h3>
                 <h1 className="text-white text-5xl font-bold  mt-2 mb-3">
@@ -17,7 +17,7 @@ const page = () => {
                     and experiences.
                 </p>
             </div>
-            <div className="grid grid-cols-2 gap-16">
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-16">
                 <Article />
                 <Article />
                 <Article />
