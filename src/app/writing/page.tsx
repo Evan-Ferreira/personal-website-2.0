4106aff98e97aac8e3d3b68018df53e8747352d9
Evan Ferreira <evanjfer@gmail.com>
2025-03-23 08:09:42 +0000

    fix: padding on writing

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className="flex flex-col items-center min-h-screen pb-16 pt-32 px-16 lg:px-48 gap-16 bg-[#2A2A2A]">
-            <div className="flex flex-col w-96 items-center">
+        <div className="flex flex-col items-center min-h-screen pb-16 pt-32 px-10 lg:px-48 gap-16 bg-[#2A2A2A]">
+            <div className="flex flex-col lg:w-96 items-center">
                 <h3 className="text-[#969696] text-md">EVAN FERREIRA</h3>
                 <h1 className="text-white text-5xl font-bold  mt-2 mb-3">
                     WRITING
