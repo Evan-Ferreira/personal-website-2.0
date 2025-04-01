e900d421e9acb811e0b395d179abe25dfd06eeaf
Evan Ferreira <evanjfer@gmail.com>
2025-04-01 20:40:20 +0100

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Host_Grotesk } from 'next/font/google';
 import './globals.css';
 import Navbar from './_components/Navbar';
+import { Analytics } from '@vercel/analytics/react';
+
 const geistSans = Geist({
     variable: '--font-geist-sans',
     subsets: ['latin'],
@@ -35,6 +37,7 @@ export default function RootLayout({
             >
                 <Navbar />
                 {children}
+                <Analytics />
             </body>
         </html>
     );
